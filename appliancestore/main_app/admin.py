9cf08b5a5b68d60157274288d9fafc27af041dd5
vsalter <vsalter1@student.gsu.edu>
2024-02-13 20:13:17 -0500

    Created superuser. Updated main_app_appliance structure. Tested ability to create object from admin panel

--- appliancestore/main_app/admin.py
+++ appliancestore/main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Appliance
+
+admin.site.register(Appliance)
 
 # Register your models here.
--- appliancestore/main_app/models.py
+++ appliancestore/main_app/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Appliance(models.Model):
-    def __init__(self, name, description, price):
-        name = models.CharField(max_length=100)
-        description = models.CharField(max_length=100)
-        price = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=100, null=True)
+    price = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
